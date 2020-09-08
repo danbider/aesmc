ff60244304c6eb2ca21632145398d18abf895b45
Dan Biderman <danbider@gmail.com>
2020-09-08 15:48:23 -0400

    added a tailored proposal

--- test/arm_models/proposal_objects.py
+++ test/arm_models/proposal_objects.py
@@ -119,9 +119,11 @@ class Learned_Proposal(nn.Module):
         at time=0, torch.tensor([batch_size, dim_latents]) 
         and at time!=0, torch.tensor([batch_size, num_particles, dim_latents])"""
     def __init__(self, initial_instance, 
-                 transition_instance, 
-                 num_hidden_units,
-                 dim_obs):
+                 transition_instance,
+                 h_type = None,
+                 num_hidden_units=None,
+                 dim_obs=None,
+                 num_timesteps=None):
         super(Learned_Proposal, self).__init__()
         # x_{t-1} = previous latents.
         # a(x_{t-1}) = current loc of transition.
@@ -138,27 +140,50 @@ class Learned_Proposal(nn.Module):
 
         self.dim_latents = self.sigma_squared_t.shape[0]
         self.dim_obs = dim_obs
+        self.h_type = h_type
+        print('our proposal is q=f*h, and type of h: %s' % self.h_type)
         
-        self.FF_mu = nn.Sequential(
-                nn.Linear(self.dim_obs, self.num_hidden_units),
-                nn.ReLU(),
-                nn.Linear(self.num_hidden_units,self.dim_latents)
-                ) # observations[t] -> mu[t]
+        if self.h_type == 'mlp':
+            
+            self.FF_mu = nn.Sequential(
+                    nn.Linear(self.dim_obs, self.num_hidden_units),
+                    nn.ReLU(),
+                    nn.Linear(self.num_hidden_units,self.dim_latents)
+                    ) # observations[t] -> mu[t]
+            
+            self.FF_var = nn.Sequential(
+                    nn.Linear(self.dim_obs, self.num_hidden_units),
+                    nn.ReLU(),
+                    nn.Linear(self.num_hidden_units,self.dim_latents),
+                    nn.Softplus()
+                    ) # observations[t] -> sigma_squared[t]
+            
+            # input will be a double, so adapt model
+            # see https://github.com/pytorch/pytorch/issues/2138
+            self.FF_mu.double()
+            self.FF_mu.to(device)
+    
+            self.FF_var.double()
+            self.FF_var.to(device)
+            
+        elif self.h_type == 'tailored':
+            self.num_timesteps = num_timesteps
+            # rows: dims; cols: timesteps
+            self.mu_mat = torch.nn.Parameter(torch.tensor(np.random.normal(loc=0.0, 
+                                                        scale=1.0, 
+                                                        size=(self.dim_latents, self.num_timesteps)),
+                                       requires_grad=True, dtype = torch.double, device = device))
+            self.sigma_mat = torch.nn.Parameter(torch.tensor(np.random.normal(loc=0.0, 
+                                                        scale=1.0, 
+                                                        size=(self.dim_latents, self.num_timesteps)),
+                                       requires_grad=True, dtype = torch.double, device = device))
+            
+            
+        elif self.h_type == 'bi-LSTM':
+            print('bi-LSTM')
         
-        self.FF_var = nn.Sequential(
-                nn.Linear(self.dim_obs, self.num_hidden_units),
-                nn.ReLU(),
-                nn.Linear(self.num_hidden_units,self.dim_latents),
-                nn.Softplus()
-                ) # observations[t] -> sigma_squared[t]
-        
-        # input will be a double, so adapt model
-        # see https://github.com/pytorch/pytorch/issues/2138
-        self.FF_mu.double()
-        self.FF_mu.to(device)
-
-        self.FF_var.double()
-        self.FF_var.to(device)
+        else:
+            print('h type not supported')
         
     
     @staticmethod
@@ -182,9 +207,28 @@ class Learned_Proposal(nn.Module):
 
     def forward(self, previous_latents=None, time=None, observations=None):
         
-        sigma_squared_star = torch.clamp(self.FF_var(observations[time]),
-                                         min = 0.01, max=30.0)
-        mu_star = self.FF_mu(observations[time])
+        if self.h_type == 'mlp':
+        
+            sigma_squared_star = torch.clamp(self.FF_var(observations[time]),
+                                             min = 0.01, max=30.0)
+            mu_star = self.FF_mu(observations[time])
+        
+        elif self.h_type == 'tailored':
+            '''take the current column of the param mat, and expand to (batch_size,dim_latents)'''
+            sigma_squared_star = torch.clamp(self.sigma_mat[:, time], 
+                                             min=0.01, max=100.0).expand(
+                                                 observations.shape[0])
+            mu_star = torch.clamp(self.mu_mat[:, time], 
+                                             min=-400.0, max=400.0).expand(
+                                                 observations.shape[0])
+            
+        elif self.h_type == 'bi-LSTM':
+            if time == 0:
+                print('run bi-LSTM for all time steps')
+            print('now for t=0:T, take readouts to be mu and sigma.')
+        
+        else:
+            print('h_type not supported.')
             
         assert(torch.sum(torch.isnan(mu_star))==0)
         assert(torch.sum(torch.isnan(sigma_squared_star))==0)
@@ -244,3 +288,143 @@ class Learned_Proposal(nn.Module):
                         aesmc.state.BatchShapeMode.FULLY_EXPANDED)
                   
 
+## Bellow is working perfectly fine. we just want to add more flexibility. 
+# class Learned_Proposal(nn.Module):
+#     """This Proposal uses a FF mapping between (1) observations[t] -> mu[t]
+#     and observations[t]} -> var[t].
+#     The weights and biases of each mapping could be learned. 
+#     Args:
+#         previous_latents: list of len num_timesteps, each entry is 
+#             torch.tensor([batch_size, num_particles, dim_latents])
+#         time: integer
+#         observations: list of len num_timesteps. each entry is a
+#         torch.tensor([batch_size, dim_observations]
+#     Returns:
+#         torch.distributions.Normal object. 
+#         at time=0, torch.tensor([batch_size, dim_latents]) 
+#         and at time!=0, torch.tensor([batch_size, num_particles, dim_latents])"""
+#     def __init__(self, initial_instance, 
+#                  transition_instance, 
+#                  num_hidden_units,
+#                  dim_obs):
+#         super(Learned_Proposal, self).__init__()
+#         # x_{t-1} = previous latents.
+#         # a(x_{t-1}) = current loc of transition.
+#         # all expressions below are tensors
+        
+#         self.num_hidden_units = num_hidden_units # for FF nns
+#         # initial distribution parameters.
+#         self.sigma_squared_0 = torch.diag(initial_instance.cov_mat)
+#         self.mu_0 = initial_instance.loc 
+        
+#         # transition dist. params.
+#         self.sigma_squared_t = torch.diag(transition_instance.diag_mat)
+#         self.transition = transition_instance
+
+#         self.dim_latents = self.sigma_squared_t.shape[0]
+#         self.dim_obs = dim_obs
+        
+#         self.FF_mu = nn.Sequential(
+#                 nn.Linear(self.dim_obs, self.num_hidden_units),
+#                 nn.ReLU(),
+#                 nn.Linear(self.num_hidden_units,self.dim_latents)
+#                 ) # observations[t] -> mu[t]
+        
+#         self.FF_var = nn.Sequential(
+#                 nn.Linear(self.dim_obs, self.num_hidden_units),
+#                 nn.ReLU(),
+#                 nn.Linear(self.num_hidden_units,self.dim_latents),
+#                 nn.Softplus()
+#                 ) # observations[t] -> sigma_squared[t]
+        
+#         # input will be a double, so adapt model
+#         # see https://github.com/pytorch/pytorch/issues/2138
+#         self.FF_mu.double()
+#         self.FF_mu.to(device)
+
+#         self.FF_var.double()
+#         self.FF_var.to(device)
+        
+    
+#     @staticmethod
+#     def get_sigma_squared_from_inverses(model_sigma_squared, sigma_squared_star):
+#         proposed_precision_vec = 1.0/model_sigma_squared + 1.0/sigma_squared_star
+#         return 1.0/proposed_precision_vec
+    
+#     @staticmethod
+#     def get_mu(proposed_sigma_squared, model_sigma_squared, 
+#                model_mu, sigma_squared_star, mu_star):
+#         proposed_mu = proposed_sigma_squared*( \
+#             (1.0/model_sigma_squared)*model_mu + \
+#             (1.0/sigma_squared_star)*mu_star)
+#         return proposed_mu
+    
+#     # @staticmethod
+#     # def expand_tensor(tensor, num_particles):
+#     #     batch_size = tensor.shape[0]
+#     #     dim = tensor.shape[1]
+        
+
+#     def forward(self, previous_latents=None, time=None, observations=None):
+        
+#         sigma_squared_star = torch.clamp(self.FF_var(observations[time]),
+#                                          min = 0.01, max=30.0)
+#         mu_star = self.FF_mu(observations[time])
+            
+#         assert(torch.sum(torch.isnan(mu_star))==0)
+#         assert(torch.sum(torch.isnan(sigma_squared_star))==0)
+        
+#         if time == 0:
+#             self.batch_size = observations[0].shape[0]
+            
+#             proposed_sigma_squared = self.get_sigma_squared_from_inverses(
+#                 self.sigma_squared_0, 
+#                 sigma_squared_star)
+            
+#             proposed_mu = self.get_mu(
+#                 proposed_sigma_squared, self.sigma_squared_0, 
+#                 self.mu_0, sigma_squared_star, mu_star
+#                 )
+
+#             return aesmc.state.set_batch_shape_mode(
+#                         torch.distributions.Normal(
+#                             loc=proposed_mu,
+#                             scale=torch.sqrt(proposed_sigma_squared)),
+#                         aesmc.state.BatchShapeMode.BATCH_EXPANDED)
+#         else: # time > 0
+#             if time == 1:
+#                 self.batch_size, self.num_particles, self.dim_latents = previous_latents[-1].shape
+            
+#             fully_expanded_dims = [self.batch_size, 
+#                         self.num_particles, 
+#                         self.dim_latents]
+            
+#             # transition sigma_squared
+#             sigma_squared_t_expanded = self.sigma_squared_t.unsqueeze(0).unsqueeze(0).expand(fully_expanded_dims)
+            
+#             sigma_squared_star_expanded = sigma_squared_star.unsqueeze(1).expand(
+#                 fully_expanded_dims)
+            
+#             mu_star_expanded = mu_star.unsqueeze(1).expand(
+#                 fully_expanded_dims)
+            
+#             proposed_sigma_squared = self.get_sigma_squared_from_inverses(
+#                 sigma_squared_t_expanded, 
+#                 sigma_squared_star_expanded)
+            
+#             # mu_t is the deterministic forward dynamics
+#             mu_t = self.transition.arm_model.forward(
+#                 previous_latents)
+                        
+#             proposed_mu = self.get_mu(
+#                 proposed_sigma_squared, sigma_squared_t_expanded, 
+#                 mu_t, sigma_squared_star_expanded, mu_star_expanded
+#                 )
+            
+            
+#             return aesmc.state.set_batch_shape_mode(
+#                         torch.distributions.Normal(
+#                             loc=proposed_mu,
+#                             scale=torch.sqrt(proposed_sigma_squared)),
+#                         aesmc.state.BatchShapeMode.FULLY_EXPANDED)
+                  
